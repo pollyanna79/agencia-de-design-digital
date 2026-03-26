b3aa2d140674fab929bafc33baafe2f4b2827666
Pollyanna santos souza <141653239+pollyanna79@users.noreply.github.com>
2026-03-26 15:42:28 -0300

    Alterando a estilização

--- src/app/Card/Card.jsx
+++ src/app/Card/Card.jsx
@@ -1,15 +1,30 @@
+'use client';
+import { useState } from 'react';
 import estilos from './Card.module.css';
 
 
 export default function Cards(props){
-    return(
-        <div className={props.ehTemaEscuro ? estilos.card_modo_escuro : estilos.card_modo_claro}>
-        <div className={estilos.container}>
-        <span>{props.data}</span>
-        <h3>{props.titulo}</h3>
-        <small>{props.empresa}</small>
-       </div>
-       <p>{props.paragrafo}</p>
-       </div>
-    )
+    const [foiClicado, setFoiClicado] = require('react').useState(false);
+   // Lógica para decidir qual classe aplicar
+    const obterClasseCard = () => {
+        if (props.ehTemaEscuro) {
+            return foiClicado ? estilos.card_selecionado_escuro : estilos.card_modo_escuro;
+        } else {
+            return foiClicado ? estilos.card_selecionado_claro : estilos.card_modo_claro;
+        }
+    };
+
+    return (
+        <div 
+            onClick={() => setFoiClicado(!foiClicado)} 
+            className={`${estilos.card_base} ${obterClasseCard()}`}
+        >
+            <div className={estilos.container}>
+                <span>{props.data}</span>
+                <h3>{props.titulo}</h3>
+                <small>{props.empresa}</small>
+            </div>
+            <p>{props.paragrafo}</p>
+        </div>
+    );
 }
